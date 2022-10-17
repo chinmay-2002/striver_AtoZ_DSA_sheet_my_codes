059f01b322397af7c4ba01d1636cf60a2648e08f
Chinmay <chinmayp100@gmail.com>
2022-10-17 16:47:10 +0530

    Added new items

---
+++ Binary Search/Bs on 1D Array/implementLower.cpp
@@ -0,0 +1,73 @@
+/* 
+Floor in a Sorted Array
+EasyAccuracy: 47.96%Submissions: 77918Points: 2
+LampGeek Week 2022 is LIVE! Click Here to View All the Exciting Offers!
+Given a sorted array arr[] of size N without duplicates, and given a value x. Floor of x is defined as the largest element K in arr[] such that K is smaller than or equal to x. Find the index of K(0-based indexing).
+
+Example 1:
+
+Input:
+N = 7, x = 0 
+arr[] = {1,2,8,10,11,12,19}
+Output: -1
+Explanation: No element less 
+than 0 is found. So output 
+is "-1".
+Example 2:
+
+Input:
+N = 7, x = 5 
+arr[] = {1,2,8,10,11,12,19}
+Output: 1
+Explanation: Largest Number less than 5 is
+2 (i.e K = 2), whose index is 1(0-based 
+indexing).
+Your Task:
+The task is to complete the function findFloor() which returns an integer denoting the index value of K or return -1 if there isn't any such number.
+
+Expected Time Complexity: O(log N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ N ≤ 107
+1 ≤ arr[i] ≤ 1018
+0 ≤ X ≤ arr[n-1]
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        long long s = 0, e = n-1;
+        long long int ans = -1;
+        long long m;
+        while(s<=e){
+            m = (e+s)/2;
+            if(v[m]==x){
+                return m;
+            }
+            else if(v[m]>x){
+                e = m-1;
+            }
+            else{
+                s = m+1;
+                ans = m;
+            }
+        }
+        return ans;
+
+        
+    }
+};
+
+
